7016fdcba075e46fe597db946bdbce79de42154c
Utkarsh Rajoriya <utkarshrajoriya00@gmail.com>
2025-09-21 12:32:26 +0530

    updated

--- src/main/java/com/utkarsh/MaterialHub/daos/PendingTeacherRepo.java
+++ src/main/java/com/utkarsh/MaterialHub/daos/PendingTeacherRepo.java
@@ -2,9 +2,11 @@ package com.utkarsh.MaterialHub.daos;
 
 import com.utkarsh.MaterialHub.models.PendingTeacher;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PendingTeacherRepo extends MongoRepository<PendingTeacher, String> {
     boolean existsByEmail(String email);
     Optional<PendingTeacher> findByEmail(String email);
